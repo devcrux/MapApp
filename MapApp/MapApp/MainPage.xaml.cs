f775c97f407df975e099d60d7f484878b32427ba
Oludayo Alli <sunnymexy@gmail.com>
2019-06-16 17:06:27 +0100

    Add project files.

---
+++ MapApp/MapApp/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Task.Delay(2000);
+            UpdateMap();
+        }
+
+
+        List<Place> placesList = new List<Place>();
+
+        private async void UpdateMap()
+        {
+            try
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream("MapApp.Places.json");
+                string text = string.Empty;
+                using (var reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                var resultObject = JsonConvert.DeserializeObject<Places>(text);
+
+                foreach (var place in resultObject.results)
+                {
+                    placesList.Add(new Place
+                    {
+                        PlaceName = place.name,
+                        Address = place.vicinity,
+                        Location = place.geometry.location,
+                        Position = new Position(place.geometry.location.lat, place.geometry.location.lng),
+                        //Icon = place.icon,
+                        //Distance = $"{GetDistance(lat1, lon1, place.geometry.location.lat, place.geometry.location.lng, DistanceUnit.Kiliometers).ToString("N2")}km",
+                        //OpenNow = GetOpenHours(place?.opening_hours?.open_now)
+                    });
+                }
+
+                MyMap.ItemsSource = placesList;
+                //PlacesListView.ItemsSource = placesList;
+                //var loc = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(47.6370891183, -122.123736172), Distance.FromKilometers(100)));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+
+        }
+    }
+}
---
+++ MapApp/MapApp/Places.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace MapApp
+{
+    public class Place
+    {
+        public string PlaceName { get; set; }
+        public string Address { get; set; }
+        public string Icon { get; set; }
+        public string Distance { get; set; }
+        public string OpenNow { get; set; }
+        public Position Position { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class Places
+    {
+        public object[] html_attributions { get; set; }
+        public string next_page_token { get; set; }
+        public Result[] results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Result
+    {
+        public Geometry geometry { get; set; }
+        public string icon { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public Opening_Hours opening_hours { get; set; }
+        public Photo[] photos { get; set; }
+        public string place_id { get; set; }
+        public Plus_Code plus_code { get; set; }
+        public float rating { get; set; }
+        public string reference { get; set; }
+        public string[] types { get; set; }
+        public int user_ratings_total { get; set; }
+        public string vicinity { get; set; }
+        public string scope { get; set; }
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+        public Viewport viewport { get; set; }
+    }
+
+    public class Location
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Northeast
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Opening_Hours
+    {
+        public bool open_now { get; set; }
+    }
+
+    public class Plus_Code
+    {
+        public string compound_code { get; set; }
+        public string global_code { get; set; }
+    }
+
+    public class Photo
+    {
+        public int height { get; set; }
+        public string[] html_attributions { get; set; }
+        public string photo_reference { get; set; }
+        public int width { get; set; }
+    }
+
+}
